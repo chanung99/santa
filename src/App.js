ec6f00c22507a59e37f7ac6602006ef8c4cb0ae4
chanung <chanung5722004@gmail.com>
2020-05-17 00:49:47 +0900

    lading
    loading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Loding from './javascriptfolder/loding';
 function App() {
   return (
     <div className="App">
@@ -10,4 +10,5 @@ function App() {
   );
 }
 
+<Loding></Loding>
 export default App;
--- src/javascriptfolder/loding.js
+++ src/javascriptfolder/loding.js
@@ -2,3 +2,5 @@ window.onload = pageLoad;
 function pageLoad(){
     alert("하하");
 }
+
+export default Loding;
